e20ee237ee578e6c2e30e368b601eecd88cd50e9
etopiei <lj343@icloud.com>
2019-07-29 23:55:35 +1000

    More focussed and more content

--- src/App.svelte
+++ src/App.svelte
@@ -40,19 +40,23 @@
 
 <p>My name is really Lachlan Jacob, or as many know me: LJ. </p>
 <p>This website is going to be a place for me to showcase some of my work, I was going to spend a long time redo-ing this website and fully kitting it out with as many npm modules as I could cram in, but instead I've opted for a more basic approach, which will allow me to focus on working on exciting projects. </p>
-<p>I've picked Svelte for my website because I really like the way it feels and it's simplicity. Stay tuned for more content coming soon.</p>
 
 <h3 class="squash">Projects</h3>
 <ul class="no-indent">
+	<h4 class="no-indent">Completed</h4>
+	<li><a href="https://github.com/etopiei/QWOP-Bot" class="no-indent">QWOP Bot</a> - A genetic algorithm that learns to play QWOP, powered by websockets and python.</li>
+	<li><a href="https://github.com/etopiei/mockapi-cli" class="no-indent">Mockapi CLI</a> - A command line utility to develop mock services quickly </li>
+	<li><a href="https://github.com/etopiei/blog" class="no-indent">Blog</a> - The software to run my blog.</li>
+	<li><a href="https://github.com/etopiei/website" class="no-indent">Website</a> - The code for this website</li>
+	<li><a href="https://github.com/etopiei/lumino" class="no-indent">Lumino</a> - A hackathon project to make Public Transport trips safer.</li>
+	<li><a href="https://github.com/etopiei/DSPM" class="no-indent">DSPM</a> (Dead Simple Password Manager) - A python CLI app to manage and generate passwords.</li>
 	<h4 class="no-indent">Coming Soon or WIP</h4>
 	<li><a class="no-indent" href="https://github.com/etopiei/kayo/">Kayo App</a> - Desktop app for watching sport with Kayo.</li>
 	<li>Open Chess Browser - A chess game explorer built with Rust and Neo4j (Coming Soon) </li>
-	<h4 class="no-indent">Completed</h4>
-	<li><a href="https://github.com/etopiei/mockapi-cli" class="no-indent">Mockapi CLI</a> - A command line utility to develop mock services quickly </li>
-	<li><a href="https://github.com/etopiei/blog" class="no-indent">Blog</a> - The software to run my blog.</li>
 </ul>
 
 <h3>Work</h3>
+<p>Below is a list of current work I am doing, as well as some of the technolgies I use in these jobs. Check out my <a class="no-indent" href="https://www.linkedin.com/in/lachlan-jacob-a14895117/">LinkedIn</a> for a more in depth view of my work exerience, education etc.</p>
 <ul class="no-indent">
 	<h4 class="no-indent">National Australia Bank</h4>
 		<li> Intern Software Engineer - January 2019 -> Present</li>
@@ -60,12 +64,6 @@
 	<h4 class="no-indent">AI Media</h4>
 		<li>Software Developer - July 2018 -> Present</li>
 		<li>PHP, Vue, MYSQL, Python, Linux Admin, AWS, cPanel</li>
-</ul>
-
-<h3>Education</h3>
-<ul class="no-indent">
-	<h4 class="no-indent">Monash University</h4>
-	<li>Bachelor of Computer Science - 2017 -> Present</li>
 </ul>
 
 <footer class="no-indent">
